68e1babedeae3af8860f02d53d4eb925614977b9
qirongxu <15062872214@163.com>
2022-04-05 22:58:59 +0800

    python第一天202204052245

--- qrx_08_字符串的格式化2.py
+++ qrx_08_字符串的格式化2.py
@@ -2,4 +2,4 @@ name = "jack"
 age = 29
 height = 1.721
 stu_num = 12
-print(f"我的名字是12：{name},我的年龄是：{age},身高是：{height:.02f},学号是：{stu_num:06d}")
+print(f"我的名字是112：{name},我的年龄是：{age},身高是：{height:.02f},学号是：{stu_num:06d}")
