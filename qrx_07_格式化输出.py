0b894b6a615f2b90ad31ea820198ab2751d635e5
qirongxu <15062872214@163.com>
2022-04-07 22:29:09 +0800

    python第一天202204072229

--- qrx_07_格式化输出.py
+++ qrx_07_格式化输出.py
@@ -6,7 +6,7 @@ age1 = int(age)
 print("我的姓名是：%s, 年龄是：%d,身高是：%.2f米" % (name, age1, height))  # 两位小数
 
 # 学号是000077
-stu_num = 77
+stu_num = 777
 print("学号是：%07d" % stu_num)  # %0nd,n表示整数有几位
 # 及格率为90%
 num = 90
